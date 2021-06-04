666ee56d4e0f9572b4eee4f6a3f3f28f2f67a25f
JiangJia <lukelmouse@qq.com>
2021-06-04 20:57:52 +0800

    update

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # Hammerspoon-X
-Hammerspoon-X
+
+Mac 上的快捷指令
+
+### **Hammerspoon安装**
+
+ `brew install hammerspoon`
+
+
+
+### **配置**
+
+![image-20210604205614991](/Users/jiangjia/Documents/Projects/Hammerspoon-X/README.assets/image-20210604205614991.png)
+
+
+
+把项目中的 `init.lua` 复制到 Config 里面即可
+
+
+
+### **快捷键**
+
+默认组合键 mash =「Control + Option + Command」
+
+当前窗口全屏     `mash + m`
+
+当前窗口最大化 `mash + l`
+
+当前窗口半屏幕显示 `mash +（方向键⬆️⬇️⬅️➡️）`
+
---
+++ init.lua
@@ -0,0 +1,28 @@
+hs.window.animationDuration = 0
+mash = { 'ctrl', 'alt', 'cmd' }
+units = {
+  right50       = { x = 0.50, y = 0.00, w = 0.50, h = 1.00 },
+  left50        = { x = 0.00, y = 0.00, w = 0.50, h = 1.00 },
+  top50         = { x = 0.00, y = 0.00, w = 1.00, h = 0.50 },
+  bot50         = { x = 0.00, y = 0.50, w = 1.00, h = 0.50 },
+  maximum       = { x = 0.00, y = 0.00, w = 1.00, h = 1.00 }
+}
+
+
+hs.hotkey.bind(mash, 'n', function()
+  local win = hs.window.focusedWindow()
+  -- get the screen where the focused window is displayed, a.k.a. current screen
+  local screen = win:screen()
+  -- compute the unitRect of the focused window relative to the current screen
+  -- and move the window to the next screen setting the same unitRect
+  win:move(win:frame():toUnitRect(screen:frame()), screen:next(), true, 0)
+end)
+
+hs.hotkey.bind(mash, 'right', function() hs.window.focusedWindow():move(units.right50, nil, true) end)
+hs.hotkey.bind(mash, 'left', function() hs.window.focusedWindow():move(units.left50, nil, true) end)
+hs.hotkey.bind(mash, 'up', function() hs.window.focusedWindow():move(units.top50, nil, true) end)
+hs.hotkey.bind(mash, 'down', function() hs.window.focusedWindow():move(units.bot50, nil, true) end)
+hs.hotkey.bind(mash, 'l', function() hs.window.focusedWindow():move(units.maximum, nil, true) end)
+hs.hotkey.bind(mash, 'm', function() hs.window.focusedWindow():toggleFullScreen() end)
+
+
