bf0b268024017cc247ff5733fc0f41e5f1ae7897
Jiang Jia <lukelmouse@qq.com>
2022-07-25 10:55:40 +0800

    Update init.lua
    center mash + p

--- init.lua
+++ init.lua
@@ -5,7 +5,8 @@ units = {
   left50        = { x = 0.00, y = 0.00, w = 0.50, h = 1.00 },
   top50         = { x = 0.00, y = 0.00, w = 1.00, h = 0.50 },
   bot50         = { x = 0.00, y = 0.50, w = 1.00, h = 0.50 },
-  maximum       = { x = 0.00, y = 0.00, w = 1.00, h = 1.00 }
+  maximum       = { x = 0.00, y = 0.00, w = 1.00, h = 1.00 },
+  center        = { x = 0.25, y = 0.25, w = 0.6 ,h = 0.6 }
 }
 
 
@@ -24,5 +25,4 @@ hs.hotkey.bind(mash, 'up', function() hs.window.focusedWindow():move(units.top50
 hs.hotkey.bind(mash, 'down', function() hs.window.focusedWindow():move(units.bot50, nil, true) end)
 hs.hotkey.bind(mash, 'l', function() hs.window.focusedWindow():move(units.maximum, nil, true) end)
 hs.hotkey.bind(mash, 'm', function() hs.window.focusedWindow():toggleFullScreen() end)
-
-
+hs.hotkey.bind(mash, 'p', function() hs.window.focusedWindow():move(units.center, nil, true) end)
